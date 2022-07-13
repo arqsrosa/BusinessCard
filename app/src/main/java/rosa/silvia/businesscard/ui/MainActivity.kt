fb014c2c26e3fbe6f23870e59524ffde984f1279
Silvia Rosa Domingues <arqsrosa@gmail.com>
2022-07-13 20:47:39 -0300

    Colcoando icone e botao
    Nessa etapa coloquei icone, botao e mudei seu tema

--- app/src/main/java/rosa/silvia/businesscard/ui/MainActivity.kt
+++ app/src/main/java/rosa/silvia/businesscard/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package rosa.silvia.businesscard.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import rosa.silvia.businesscard.R
+import rosa.silvia.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
