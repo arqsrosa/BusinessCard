052a21b678ac1b107fbac7b8b39f7b6ecd454958
Silvia Rosa Domingues <arqsrosa@gmail.com>
2022-07-14 17:14:53 -0300

    atualizando codigo e dando vida os botoes

--- app/src/main/java/rosa/silvia/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/rosa/silvia/businesscard/ui/AddBusinessCardActivity.kt
@@ -1,11 +1,24 @@
 package rosa.silvia.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import rosa.silvia.businesscard.databinding.ActivityAddBusinessCardBinding
+import rosa.silvia.businesscard.databinding.ActivityMainBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_business_card)
+        setContentView(binding.root)
+        insertListener()
     }
+
+    private fun insertListener () {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+
 }
